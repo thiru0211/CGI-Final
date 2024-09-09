cbbc23982f73681078f83d13b4c2bf1ac3e01c3e
thiru0211 <0211thiru@gmail.com>
2024-09-09 10:25:01 +0530

    New commit

--- src/test/java/allPages/ReRunFailedTestCase.java
+++ src/test/java/allPages/ReRunFailedTestCase.java
@@ -5,7 +5,7 @@ import org.testng.ITestResult;
 
 public class ReRunFailedTestCase implements IRetryAnalyzer{
 	private int retryCount=0;
-	private static final int maxCount=2
+	private static final int maxCount=1
 			;
 	@Override
 	public boolean retry(ITestResult result) {
